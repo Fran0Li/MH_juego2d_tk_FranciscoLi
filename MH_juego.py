a032400f6cc4a4ce6a2fefaa59a54a1420f820a3
Francisco Li <liruizfrancisco@gmail.com>
2026-04-26 17:36:12 -0600

    Importación de librerías y se establecen constantes de la ventana

---
+++ MH_juego.py
@@ -0,0 +1,11 @@
+import tkinter as tk#importación de la librería tkinter y se nombra como tk
+import pygame as py#importación de la librería pygame para el audio
+import math#importación de la librería de mate para su uso posterior
+
+#Constantes para la ventana
+ANCHO_VP = 800 #ancho de la ventana principal
+ALTO_VP = 600 #alto de la ventana prrincipal
+GRAVEDAD = 0.8#valor de gravedad
+POTENCIA_JUMP = -15 #potencia del salto, es negativo porque arriba es restar en Y
+VELOCIDAD_MOV = 6 #velocidad de movimiento
+
