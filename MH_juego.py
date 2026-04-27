fdb05502b46eb2a47473c5c91c158d0f236a7466
Francisco Li <liruizfrancisco@gmail.com>
2026-04-26 23:05:03 -0600

    Funcion de animacion, creacion de ventana principal y binds de movimiento
    También se hizo una corrección en "space" por que daba un problema de salto infinito

--- MH_juego.py
+++ MH_juego.py
@@ -20,7 +20,7 @@ hunter = [100, 100, 0, False, None]
 #i[4]: identidad del dibuj en el Canvas (ID)
 
 #interruptores de las teclas para un movimiento fluido 
-teclas = {"Left": False, "Right": False, "Space": False}
+teclas = {"Left": False, "Right": False, "space": False}
 #En orden respectivo de izquierda a derecha
 #Estado de la flecha izquierda o a, estado de la flecha derecha o d, estado de la tecla espacio o W
 
@@ -30,7 +30,7 @@ def izquier(event):
 def derecha(event):
     teclas["Right"] = True#Lo mismo que el de arriba pero para la derecha
 def salto(event):
-    teclas["Space"] = True#Lo mismo pero hacia arriba, el salto
+    teclas["space"] = True#Lo mismo pero hacia arriba, el salto
 
 def detener_key(event):#Funcion para detener el interruptor
     k = event.keysym #captura nombre de la tecla que el usuario dejó de presionar
@@ -38,8 +38,8 @@ def detener_key(event):#Funcion para detener el interruptor
         teclas["Left"] = False # apaga el interruptor de izq
     elif k in ["Right", "d", "D"]:# si fue ir a la derecha 
         teclas["Right"] = False #apaga interruptor de derecha
-    elif k in ["Space", "w", "W"]:# si fue salto, arriba
-        teclas["Space"] = False # apaga interruptor de salto
+    elif k in ["space", "w", "W"]:# si fue salto, arriba
+        teclas["space"] = False # apaga interruptor de salto
 
 #Lógica del mov!!
 
@@ -51,7 +51,7 @@ def mover_hunter():
         hunter[0] += VELOCIDAD_MOV# se mueve a la derecha a la v de mov
     
     #Lógica de salto
-    if teclas["Space"] and hunter[3]:
+    if teclas["space"] and hunter[3]:
         hunter[2] = POTENCIA_JUMP # Velocidad vertical hacia arriba
         hunter[3] = False #desactiva el estado de que está en el suelo
 
@@ -69,3 +69,40 @@ def mover_hunter():
         hunter[2] = 0 #devuelve la velocidad de caida a cero
         hunter[3] = True #marca que ahora está tocando el suelo
 
+#Animacion en ventana
+
+def animove():
+    mover_hunter() # Ejecuta la lógica de movimiento y salto
+
+    #Actualiza el dibujo del personaje usando las coordenadas de  la lista de hunter
+    #canva.coords usa: (ID, x1, y1, x2, y2)
+    canvas.coords(hunter[4], hunter[0], hunter[1], hunter[0] + 30, hunter[1]+ 40)
+    #los valores respectivamente son, el ID, esquina superior derecha, esquina inferior derecha (x+ancho, y+alto)
+    ventana.after(20, animove)#Le dice a la ventana que vuelva a correr esta función en 20 ms
+
+#Ventana interfazconfig
+ventana = tk.Tk()#crea base
+ventana.title("MH 2026_FranLi") #Título de la ventana
+ventana.resizable(False,False)#Para que el usuario no pueda alterar el tamaño de ventana
+
+# Area donde se dibujarán los rectángulos
+canvas = tk.Canvas(ventana, width=ANCHO_VP, height=ALTO_VP, bg="#222222")#Dimensiones y color
+canvas.pack()#Coloca el canvas dentro de la ventana
+#Dibuja un rectángulo que representa el suelo
+canvas.create_rectangle(0, ALTO_VP-20, ANCHO_VP, ALTO_VP, fill="green")
+#Dibujar a hunter(jugador)
+hunter[4] = canvas.create_rectangle(hunter[0], hunter[1], hunter[0] + 30, hunter [1] + 40, fill="purple", outline="white")#Asigna posición, tamaño y colores al dibujo
+
+#Binds, spn la vinculacion de eventos conecta las teclas fisicas con las funciones del programa
+ventana.bind("<KeyPress-Left>", izquier)#Vincula flecha izq para que active función izquier
+ventana.bind("<KeyPress-Right>", derecha)#Vincula flecha der para que active función derecha
+ventana.bind("<KeyPress-space>", salto)#Vincula espacio para que active función salto
+ventana.bind("<KeyPress-a>", izquier)#Vincula a para funcion izquier
+ventana.bind("<KeyPress-d>", derecha)#Vincula d para funcion derecha
+ventana.bind("<KeyPress-w>", salto)#Vincula w para funcion salto
+#KeyRelease es un evento, se activa cuando el usuario  levanta cualquier tecla
+ventana.bind("<KeyRelease>", detener_key)#al activarse, la funcion detener_key revisa cual fue y apaga el interruptor
+
+animove()
+
+ventana.mainloop()
